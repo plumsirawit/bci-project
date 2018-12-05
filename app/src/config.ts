bcb277fabd17d4469dd2177e4e3d745e8602764a
Sirawit Pongnakintr <plurm2545@hotmail.com>
2018-12-05 17:36:03 +0700

    Install Angular-Firebase Connector

---
+++ app/src/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    fire: {
+        apiKey: "AIzaSyAoWEI-4LrXrLM7PugFcB3QITO3JwSx2h4",
+        authDomain: "bci-chat-app.firebaseapp.com",
+        databaseURL: "https://bci-chat-app.firebaseio.com",
+        projectId: "bci-chat-app",
+        storageBucket: "bci-chat-app.appspot.com",
+        messagingSenderId: "742671350955"
+    }
+}
