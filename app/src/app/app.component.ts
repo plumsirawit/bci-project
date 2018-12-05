048faa96d61710a48c51a6cc4253275143fb9b65
Sirawit Pongnakintr <plurm2545@hotmail.com>
2018-12-05 17:26:39 +0700

    Mange Package Dependencies

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -12,12 +12,11 @@ export class MyApp {
   rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    splashScreen.show();
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
+      splashScreen.show();
     });
   }
 }
