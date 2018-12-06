83014bf0b2790bf2163d978c6bd29605577a4900
Sirawit Pongnakintr <plurm2545@hotmail.com>
2018-12-06 13:23:42 +0700

    Edit page to template

--- app/src/pages/friends/friends.ts
+++ app/src/pages/friends/friends.ts
@@ -1,25 +1,14 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
-/**
- * Generated class for the FriendsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-friends',
-  templateUrl: 'friends.html',
+  templateUrl: 'friends.html'
 })
 export class FriendsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController) {
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FriendsPage');
   }
 
 }
