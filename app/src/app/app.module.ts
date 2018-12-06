924b2b18beca16161bf8f6441188c8074f6295da
Sirawit Pongnakintr <plurm2545@hotmail.com>
2018-12-06 13:16:47 +0700

    Delete Unused Pages and Link New Pages

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
@@ -19,13 +16,16 @@ import { firebaseConfig } from '../config';
 import { AuthService } from '../services/auth.service';
 import { NgxErrorsModule } from '@ultimate/ngxerrors';
 import { SplashPage } from '../pages/splash/splash';
+import { FriendsPage } from '../pages/friends/friends';
+import { ChatsPage } from '../pages/chats/chats';
+import { AccountPage } from '../pages/account/account';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    FriendsPage,
+    ChatsPage,
+    AccountPage,
     TabsPage,
     LoginPage,
     SignupPage,
@@ -40,9 +40,9 @@ import { SplashPage } from '../pages/splash/splash';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    FriendsPage,
+    ChatsPage,
+    AccountPage,
     TabsPage,
     LoginPage,
     SignupPage,
--- app/src/pages/tabs/tabs.ts
+++ app/src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { FriendsPage } from '../friends/friends';
+import { ChatsPage } from '../chats/chats';
+import { AccountPage } from '../account/account';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = FriendsPage;
+  tab2Root = ChatsPage;
+  tab3Root = AccountPage;
 
   constructor() {
 
