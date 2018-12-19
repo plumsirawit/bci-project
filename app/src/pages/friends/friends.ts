821060fbf51be2bd79f113bcc3b716b651e58e8e
Sirawit Pongnakintr <plurm2545@hotmail.com>
2018-12-20 02:00:28 +0700

    Reformat Friends

--- app/src/pages/friends/friends.ts
+++ app/src/pages/friends/friends.ts
@@ -9,7 +9,7 @@ export class FriendsPage {
   items: Array<any>;
   constructor(public navCtrl: NavController) {
     this.items = new Array();
-    this.items.push({'name': 'OK'});
+    this.items.push({'name': 'OK', 'uid': '#000000'});
   }
 
   clicked(item: any){
